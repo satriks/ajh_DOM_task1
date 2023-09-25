dc1cf31ec3a79312da9aaa269ae78af0ecf7224e
Yury Vereskun <satriks@mail.ru>
2023-09-26 01:42:09 +0300

    fix lint + actons

--- src/js/gameControl.js
+++ src/js/gameControl.js
@@ -1,22 +1,20 @@
-import images from "../images/goblin.png"
+import images from '../images/goblin.png';
 
-document.addEventListener('DOMContentLoaded', () => game())
+document.addEventListener('DOMContentLoaded', () => game());
 
 function game() {
-    const holes = document.querySelectorAll('.hole');
-    const img = document.createElement('img');
-    img.src = images
-    getHole()
-    setInterval(getHole, 3000)
-    
-    function getHole() {
-        
-        const index = Math.floor(Math.random() * holes.length)
-        if (holes[index].childElementCount){
-            getHole()
-        }
-        img.remove()
-        holes[index].appendChild(img)
+  const holes = document.querySelectorAll('.hole');
+  const img = document.createElement('img');
+  img.src = images;
+  getHole();
+  setInterval(getHole, 3000);
+
+  function getHole() {
+    const index = Math.floor(Math.random() * holes.length);
+    if (holes[index].childElementCount) {
+      getHole();
     }
+    img.remove();
+    holes[index].appendChild(img);
+  }
 }
-    
